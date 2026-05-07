5d2aa6fcd7f01b6ed1c65b799dcd63724b3ea881
Dongmin, Yu <ydm2790@gmail.com>
2026-05-07 15:57:55 +0900

    feat: update Codegen spec with isAvailable, requestReview, openStoreListing

--- src/NativeInAppReview.ts
+++ src/NativeInAppReview.ts
@@ -1,7 +1,9 @@
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
 
 export interface Spec extends TurboModule {
-  multiply(a: number, b: number): number;
+  isAvailable(): Promise<boolean>;
+  requestReview(): Promise<void>;
+  openStoreListing(appStoreId: string | null): Promise<void>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('InAppReview');
